efd4e3e910b29568f1c2f8282aadf62142e60cd2
tle <tlepayut@gmail.com>
2017-11-30 12:35:27 +0700

    fuck u tie

--- kitchen/src/main/scala/com/example/app/MyScalatraServlet.scala
+++ kitchen/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -3,6 +3,7 @@ package com.example.app
 import org.scalatra._
 import org.json4s.jackson.Serialization
 import java.util.UUID
+import java.time.Instant
 
 import org.scalatra.json.JacksonJsonSupport
 import org.json4s.jackson.JsonMethods._
@@ -24,7 +25,7 @@ import com.mongodb.casbah.Imports.DBObject
 //import scalate.ScalateSupport
 //import net.liftweb.json._
 //case class someModel(id: Int, food: String, status: String);
-case class someModel_1(food: String,price:Int,id: Int, status: String);
+case class someModel_1(kind: String, food: String, price:Int, id: Int, status: String);
 case class model_2(UUID: String, food: String, id: Int, price: Int, status: String, trash: ObjectId)
 //case class All(list: List[someModel])
 case class whole()
@@ -53,6 +54,7 @@ class MyScalatraServlet extends ScalatraServlet  with CorsSupport  {
 
   val idpw = db("idpw")
   val main_kitchen = db("main_kitchen")
+  val cashier = db("backup")
 
 
 
@@ -74,15 +76,19 @@ class MyScalatraServlet extends ScalatraServlet  with CorsSupport  {
     def generateUniqueId = UUID.randomUUID().toString
 
     val json = request.body
+    val e = request.body.getClass
+//    println(e)
+
 
     val obj = parse(json).extract[(List[someModel_1])]
 
     for (a <- 0 to obj.size-1 ){
-      val eiei = MongoDBObject("id"->obj(a).id, "food"->obj(a).food, "status"->obj(a).status, "price"->obj(a).price, "UUID"->generateUniqueId)
+      val d = Instant.now.getEpochSecond
+      val eiei = MongoDBObject("id"->obj(a).id, "food"->obj(a).food, "status"->obj(a).status, "price"->obj(a).price, "UUID"->generateUniqueId, "kind"->obj(a).kind, "time"->d )
       main_kitchen.insert(eiei)
     }
 //    val obj = parse(json).extract[someModel]
-    println(obj.size)
+//    println(obj.size)
 
 
 
@@ -172,6 +178,13 @@ class MyScalatraServlet extends ScalatraServlet  with CorsSupport  {
     main_kitchen.update(update_1, update_2)
     Ok("updated from table")
   }
+
+  delete("/delfood/:id"){
+    val id = params("id")
+    val update_1 = MongoDBObject( "UUID"-> id )
+    main_kitchen.remove(update_1)
+
+  }
   ///////////////////////////////////////////////////////////////////////////////////////////////////
 
   get("/kitchen"){
@@ -181,7 +194,10 @@ class MyScalatraServlet extends ScalatraServlet  with CorsSupport  {
     //    compact(render(james_ex))
     //----------------------------
     //    val eiei = MongoDBObject( "id"-> 1 )
-    val allDocs = main_kitchen.find()
+
+    val eiei = MongoDBObject("kind"->"food")
+    val sorttt = MongoDBObject("time"-> 1)
+    val allDocs = main_kitchen.find(eiei).sort( sorttt)
 //    println( "kitchen" )
     //    val eiei = for(doc <- allDocs)
     //    for(doc <- allDocs) println( doc )
@@ -190,12 +206,33 @@ class MyScalatraServlet extends ScalatraServlet  with CorsSupport  {
     Ok(Serialization.write(r))
   }
 
+  get("/dessert"){
+    //render all everything in db
+    contentType = "application/json"
+    //    val james_ex = (("id"->"5") ~ ("food"->"burger") ~ ("status" -> "waiting"))
+    //    compact(render(james_ex))
+    //----------------------------
+    //    val eiei = MongoDBObject( "id"-> 1 )
+    val sorttt = MongoDBObject("time"-> 1)
+    val eiei = MongoDBObject("kind"->"dessert")
+    val allDocs = main_kitchen.find(eiei).sort(sorttt)
+    //    println( "kitchen" )
+    //    val eiei = for(doc <- allDocs)
+    //    for(doc <- allDocs) println( doc )
+
+    val r: Seq[Map[String, AnyRef]] = allDocs.toVector.map{_.toVector}.map{_.toMap}
+    Ok(Serialization.write(r))
+  }
+
+
+
   get("/eachtable/:id"){
     //assume to get table id 1
 
     val id = params("id")
     val eiei = MongoDBObject( "id"-> id.toInt )
-    val allDocss = main_kitchen.find( eiei )
+    val sort = MongoDBObject("time" -> 1)
+    val allDocss = main_kitchen.find( eiei ).sort(sort)
     val r: Seq[Map[String, AnyRef]] = allDocss.toVector.map{_.toVector}.map{_.toMap}
     Ok(Serialization.write(r))
   }
@@ -206,13 +243,86 @@ class MyScalatraServlet extends ScalatraServlet  with CorsSupport  {
 //    println(idpw.findOne(eiei))
     if (idpw.findOne(eiei) == None){
       idpw.insert(eiei)
+      Ok("true")
     }
     else{
 //      response.addHeader("status","error")
-      response.sendError(400, "error")
+      Ok("false")
+//      response.sendError(400, "error")
 //      response.
     }
   }
+
+
+
+  get("/check_out/:id"){
+    val id = params("id")
+    val eiei = MongoDBObject( "id"-> id.toInt )
+    val search = main_kitchen.find(eiei)
+
+    var ans = 0
+    var count = 0
+    for(doc <- search) {
+      var e = doc.toList(4)._2
+//      println(e.getClass())
+      ans = ans + e.asInstanceOf[Int]
+      count += 1
+    }
+
+    val amount = MongoDBObject("id"-> id.toInt , "status"->"Unpaid","amount"->ans)
+//    cashier.insert(amount)
+
+    Ok(ans)
+
+  }
+  post("/check_out_1/:id"){
+    def generateUniqueId = UUID.randomUUID().toString
+    val id = params("id")
+    val eiei = MongoDBObject( "id"-> id.toInt )
+    val search = main_kitchen.find(eiei)
+
+    var ans = 0
+    var count = 0
+    for(doc <- search) {
+      var e = doc.toList(4)._2
+      //      println(e.getClass())
+      ans = ans + e.asInstanceOf[Int]
+      count += 1
+    }
+
+    val amount = MongoDBObject("id"-> id.toInt ,"amount"->ans, "status"->"Unpaid","UUID"->generateUniqueId)
+    cashier.insert(amount)
+
+
+  }
+  delete("/check_out_2/:id"){
+    val id = params("id")
+    val eiei = MongoDBObject( "id"-> id.toInt )
+    main_kitchen.remove(eiei)
+  }
+
+  get("/cashier"){
+    contentType = "application/json"
+    val allDocs = cashier.find()
+
+    val r: Seq[Map[String, AnyRef]] = allDocs.toVector.map{_.toVector}.map{_.toMap}
+    Ok(Serialization.write(r))
+  }
+
+  post("/cashier_update/:id/:status"){
+    val id = params("id")
+    val status = params("status")
+
+    val update_1 = MongoDBObject( "UUID"-> id )
+    val update_2 = $set( "status" -> status )
+    cashier.update(update_1, update_2)
+
+    Ok("updated cashier")
+
+  }
+
+
+
 
 
 }
